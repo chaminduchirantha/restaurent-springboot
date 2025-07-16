fc0cfdf74d887a3f49f7c8dc03ef02df932d3d00
chaminduchirantha <chaminduchirantha73@gmail.com>
2025-07-16 20:43:03 +0530

    apply the js part in display the views

--- js/menu.js
+++ js/menu.js
@@ -26,4 +26,34 @@ $(document).ready(function () {
     $('#all').on('click', function () {
         filterCards('all');
     });
+
+
+    let viewCounts = JSON.parse(localStorage.getItem('viewCounts')) || {};
+
+    $(document).ready(function () {
+        $('.view-details-btn').click(function () {
+            const productId = $(this).data('id');
+            const modalId = $(this).data('modal-id');
+
+            // Increment the count
+            viewCounts[productId] = (viewCounts[productId] || 0) + 1;
+
+            // Save to localStorage
+            localStorage.setItem('viewCounts', JSON.stringify(viewCounts));
+
+            // Find and update the view count inside the correct modal
+            const modal = $('#' + modalId);
+            modal.find('.view-count[data-id="' + productId + '"]').text(viewCounts[productId]);
+        });
+
+        // On page load, update all modal view-count spans with saved data
+        $('.view-count').each(function () {
+            const id = $(this).data('id');
+            if (viewCounts[id]) {
+                $(this).text(viewCounts[id]);
+            }
+        });
+    });
 });
+
+
