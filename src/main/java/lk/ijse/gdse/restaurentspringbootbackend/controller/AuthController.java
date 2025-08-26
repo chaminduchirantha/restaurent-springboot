7262f6f38a8abec8385c922871e77380f6e58332
chaminduchirantha <chaminduchirantha73@gmail.com>
2025-08-26 08:09:01 +0530

    update the auth controller

--- src/main/java/lk/ijse/gdse/restaurentspringbootbackend/controller/AuthController.java
+++ src/main/java/lk/ijse/gdse/restaurentspringbootbackend/controller/AuthController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("auth")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:63343")
+@CrossOrigin(origins = "http://localhost:63342")
 public class AuthController {
     private final AuthService authService;
 
