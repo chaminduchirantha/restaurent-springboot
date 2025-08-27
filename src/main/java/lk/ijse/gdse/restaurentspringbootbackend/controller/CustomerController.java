46ffd78f5eec3d65166ec7f09089d53414295bfc
chaminduchirantha <chaminduchirantha73@gmail.com>
2025-08-27 12:13:03 +0530

    update the customer controller

--- src/main/java/lk/ijse/gdse/restaurentspringbootbackend/controller/CustomerController.java
+++ src/main/java/lk/ijse/gdse/restaurentspringbootbackend/controller/CustomerController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/user")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:63342")
 public class CustomerController {
     private final CustomerServiceImpl customerService;
     private final CustomerRepo userRepo;
