cfe8145130aff9c115c485044bfbb5526f7c4dcb
chaminduchirantha <chaminduchirantha73@gmail.com>
2025-08-30 21:02:04 +0530

    update the customer dto

--- src/main/java/lk/ijse/gdse/restaurentspringbootbackend/service/CustomerService.java
+++ src/main/java/lk/ijse/gdse/restaurentspringbootbackend/service/CustomerService.java
@@ -7,7 +7,7 @@ import java.util.List;
 public interface CustomerService {
     List<CustomerDto> getAllCustomer();
     CustomerDto saveCustomer(CustomerDto customerDto);
-    CustomerDto updateCustomer(Long id, CustomerDto customerDto);
+    CustomerDto updateCustomer(CustomerDto customerDto);
     void deleteCustomer(Long id);
     List<CustomerDto> getCustomersByPage(int page, int size);
     int getTotalPages(int size);
