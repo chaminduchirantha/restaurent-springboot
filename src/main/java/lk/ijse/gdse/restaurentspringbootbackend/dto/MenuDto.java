a3d687a4fd2e8dcd477eb37009aad7fad1a95244
chaminduchirantha <chaminduchirantha73@gmail.com>
2025-09-02 19:19:02 +0530

    update the menu dto

--- src/main/java/lk/ijse/gdse/restaurentspringbootbackend/dto/MenuDto.java
+++ src/main/java/lk/ijse/gdse/restaurentspringbootbackend/dto/MenuDto.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -14,6 +16,6 @@ public class MenuDto {
     private String name;
     private String category;
     private String description;
-    private String imageUrl;
+    private List<String> imageUrls;
     private double price;
 }
